4b1c31781f2c886b439ca9663085157533048824
Jakob <jakobroos8@gmail.com>
2019-01-03 17:13:35 +0100

    Vi er endnu engang live

--- src/sample/SpellController.java
+++ src/sample/SpellController.java
@@ -1,12 +1,21 @@
 package sample;
 
 import XMLHandler.Spell;
+import XMLHandler.SpellHitDie;
 import XMLHandler.XMLHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import org.controlsfx.control.textfield.TextFields;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,6 +26,8 @@ public class SpellController {
     private XMLHandler xmlh;
     private ArrayList<Spell> spells;
     private ArrayList<Spell> filteredSpellList;
+    private AutoCompletionBinding<String> autoCompletionBinding;
+
 
     public void Initialize(XMLHandler xmlh){
         this.xmlh = xmlh;
@@ -30,7 +41,32 @@ public class SpellController {
         levelFilter = new ArrayList<>();
         filteredSpellList = new ArrayList<>();
         setButtons();
+        InitializeSearchBar();
+
+
     }
+
+    private void InitializeSearchBar() {
+        ArrayList<String> spellNames = new ArrayList<>();
+        TextFields.bindAutoCompletion(SpellSearchBar, spellNames);
+        if(filteredSpellList.isEmpty()) {
+            for (Spell spell : spells) {
+                spellNames.add(spell.getName());
+            }
+            bindAutoComplete(spellNames);
+        }
+        else{
+            for (Spell spell: filteredSpellList) {
+                spellNames.add(spell.getName());
+            }
+            /**
+             * KAN GØRES PÆNERE
+             *
+             */
+           bindAutoComplete(spellNames);
+        }
+    }
+
     private void Filter(){
         filteredSpellList.clear();
         boolean isFilteredSpell = false;
@@ -56,7 +92,8 @@ public class SpellController {
             }
 
         }
-        System.out.println("hej");
+        InitializeSearchBar();
+        System.out.println(filteredSpellList);
     }
 
     /**
@@ -218,5 +255,179 @@ public class SpellController {
         setLevelButtonAction(ninethToggleButton);
 
     }
+    private void bindAutoComplete(ArrayList<String> spellNames){
+        if(autoCompletionBinding != null) {
+            autoCompletionBinding.dispose();
+        }
+        autoCompletionBinding = TextFields.bindAutoCompletion(SpellSearchBar, spellNames);
+        autoCompletionBinding.setOnAutoCompleted(event ->
+                handleCompletion(event.getCompletion()));
 
+    }
+
+    public void handleCompletion(String s) {
+
+        if(xmlh.getSpellHashMap().containsKey(s)){
+            Spell spell = xmlh.getSpellHashMap().get(s);
+            System.out.println("YO!");
+            BuildSpellPopUp(spell);
+        }
+    }
+
+    private void BuildSpellPopUp(Spell spell) {
+        GridPane gridPane = new GridPane();
+        gridPane.setMaxWidth(600);
+        int counter = 0;
+
+        if(!spell.getName().isEmpty()) {
+            Label name = new Label(spell.getName());
+            name.setFont(Font.font("Apple Braille", 20));
+            name.setStyle("-fx-text-fill: red");
+            gridPane.add(name, 0,counter,1,1);
+            counter++;
+        }
+        if(!spell.getSchool().isEmpty()) {
+
+            String levelName = "";
+
+            boolean match = false;
+            switch(spell.getLevel()) {
+                case 0: levelName = "Cantrip";
+                        match = true;
+                        break;
+                case 1: levelName = "1st-level";
+                        match = true;
+                        break;
+                case 2: levelName = "2nd-level";
+                        match = true;
+                        break;
+                case 3: levelName = "3rd-level";
+                        match = true;
+                        break;
+            }
+            if(match == false){
+                levelName = spell.getLevel()+"th-level";
+            }
+
+            Label levelAndSchool = new Label(levelName + " " + spell.getSchool());
+            levelAndSchool.setFont(Font.font("Apple Braille", FontPosture.ITALIC, 16));
+            gridPane.add(levelAndSchool,0,counter,1,1);
+            counter++;
+        }
+        if(!spell.getTime().isEmpty()) {
+            TextFlow timeFlow = new TextFlow();
+            Text text1 = new Text("Casting Time: ");
+            text1.setStyle("-fx-font-weight: bold");
+
+            Text text2 = new Text(spell.getTime());
+            text2.setStyle("-fx-font-weight: regular");
+            timeFlow.getChildren().addAll(text1,text2);
+
+            gridPane.add(timeFlow,0,counter,1,1);
+            counter++;
+        }
+        if(!spell.getRange().isEmpty()) {
+            TextFlow rangeFlow = new TextFlow();
+            Text text1 = new Text("Range: ");
+            text1.setStyle("-fx-font-weight: bold");
+
+            Text text2 = new Text(spell.getRange());
+            text2.setStyle("-fx-font-weight: regular");
+            rangeFlow.getChildren().addAll(text1,text2);
+
+            gridPane.add(rangeFlow,0,counter,1,1);
+            counter++;
+        }
+        if(!spell.getComponents().isEmpty()) {
+            TextFlow componentFlow = new TextFlow();
+            Text text1 = new Text("Components: ");
+            text1.setStyle("-fx-font-weight: bold");
+
+            String concatComponents = "";
+            for (String s: spell.getComponents()) {
+                concatComponents = concatComponents + s;
+            }
+            Text text2 = new Text(concatComponents);
+            text2.setStyle("-fx-font-weight: regular");
+            componentFlow.getChildren().addAll(text1,text2);
+
+            gridPane.add(componentFlow,0,counter,1,1);
+            counter++;
+        }
+        if(!spell.getDuration().isEmpty()) {
+            TextFlow durationFlow = new TextFlow();
+            Text text1 = new Text("Duration: ");
+            text1.setStyle("-fx-font-weight: bold");
+
+            Text text2 = new Text(spell.getDuration());
+            text2.setStyle("-fx-font-weight: regular");
+            durationFlow.getChildren().addAll(text1,text2);
+
+            gridPane.add(durationFlow,0,counter,1,1);
+            counter++;
+        }
+        if(!spell.getClasses().isEmpty()) {
+            TextFlow classesFlow = new TextFlow();
+            Text text1 = new Text("Classes: ");
+            text1.setStyle("-fx-font-weight: bold");
+
+            String concatClasses = "";
+            for (String s: spell.getClasses()) {
+                concatClasses = concatClasses + s;
+            }
+            Text text2 = new Text(concatClasses);
+            text2.setStyle("-fx-font-weight: regular");
+            classesFlow.getChildren().addAll(text1,text2);
+
+            gridPane.add(classesFlow,0,counter,1,1);
+            counter++;
+        }
+        if(!spell.getTexts().isEmpty()) {
+            for (String s: spell.getTexts()) {
+                Label textLabel = new Label("\n"+s);
+                textLabel.setWrapText(true);
+                gridPane.add(textLabel,0,counter,1,1);
+                counter++;
+            }
+        }
+        if(!spell.getRolls().isEmpty()){
+
+            for (SpellHitDie shd: spell.getRolls()) {
+                String roll;
+                roll = shd.getDieAmount()+"d"+shd.getDieSize()+"+"+shd.getBonus();
+                gridPane.add(new Label(roll),0,counter,1,1);
+                counter++;
+                Button rollButton = new Button("roll: " + roll);
+                rollButton.setOnAction(e -> {
+                    System.out.println("hej" + roll);
+                });
+                gridPane.add(rollButton, 1, counter, 1 ,1);
+            }
+        }
+
+
+
+
+        AnchorPane anchorPane = new AnchorPane(gridPane);
+        anchorPane.setScaleX(gridPane.getScaleX());
+        anchorPane.setScaleY(gridPane.getScaleY());
+        anchorPane.setMaxWidth(800);
+        anchorPane.setMaxHeight(2000);
+
+        ScrollPane scrollPane = new ScrollPane(anchorPane);
+        scrollPane.setScaleX(anchorPane.getScaleX());
+        scrollPane.setScaleY(anchorPane.getScaleY());
+        scrollPane.setMaxWidth(800);
+        scrollPane.setMaxHeight(1000);
+
+        BorderPane borderPane = new BorderPane(scrollPane);
+        borderPane.setScaleX(scrollPane.getScaleX());
+        borderPane.setScaleY(scrollPane.getScaleY());
+        borderPane.setMaxWidth(800);
+        borderPane.setMaxHeight(1000);
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(borderPane));
+        stage.show();
+    }
 }
